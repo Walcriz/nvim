52fb50d3c1b4a54044a42a3dfafbdad7bdaaff7e
Walcriz <walcriz.mail@gmail.com>
2023-03-09 20:24:25 +0100

    Update toggleterm.lua

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -15,13 +15,13 @@ return {
 				-- },
 
 				terminal = {
-					term_opts = {},
+					term_opts = {
+						cmd = "powershell",
+					},
 					toggle_key = "<leader>te",
 				},
 			},
 			defaults = {
-				direction = "float",
-				winblend = 0,
 				highlights = {
 					border = "TelescopeNormal",
 					background = "TelescopeNormal",
@@ -29,28 +29,26 @@ return {
 			},
 		},
 		keys = {
-			{ "<C-W>:", [[<Cmd>wincmd h<CR>]], mode = "t" },
-
 			{ "<C-W>h", [[<Cmd>wincmd h<CR>]], mode = "t" },
 			{ "<C-W>j", [[<Cmd>wincmd j<CR>]], mode = "t" },
 			{ "<C-W>k", [[<Cmd>wincmd k<CR>]], mode = "t" },
 			{ "<C-W>l", [[<Cmd>wincmd l<CR>]], mode = "t" },
-			{ "<C-h>", [[<Cmd>wincmd h<CR>]], mode = "t" },
-			{ "<C-j>", [[<Cmd>wincmd j<CR>]], mode = "t" },
-			{ "<C-k>", [[<Cmd>wincmd k<CR>]], mode = "t" },
-			{ "<C-l>", [[<Cmd>wincmd l<CR>]], mode = "t" },
 		},
 		config = function(_, opts)
 			require("toggleterm").setup()
+			local util = require("lazy.core.util")
 
 			local Terminal = require("toggleterm.terminal").Terminal
 
 			for tool, options in pairs(opts.tools) do
-				local term_opts = vim.deepcopy(opts.defaults)
-                -- stylua: ignore
-                for k,v in pairs(options.term_opts) do term_opts[k] = v end
+				local term_opts = opts.defaults
+				for k, v in pairs(options.term_opts) do
+					term_opts[k] = v
+				end
 
-				local tool_terminal = Terminal:new(options.term_opts)
+                -- stylua: ignore
+
+				local tool_terminal = Terminal:new(term_opts)
 
 				function toggle()
 					tool_terminal:toggle()

d0910f9d0055ba82504d3f9a60e024ca8453f326
Walcriz <walcriz.mail@gmail.com>
2023-03-19 16:41:31 +0100

    Make CTRL-z not freeze neovim

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
 local init = require("init")
 
+-- Avoid freezing vim forever
+if vim.fn.has("win32") then
+	vim.keymap.set("n", "C-z", "<Nop>")
+end
+
 -- Initial setup and bootstrapping
 init.init()
 

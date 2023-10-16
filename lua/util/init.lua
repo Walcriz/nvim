b66dab1f9d092feb1967ae09ebadaf66020450f7
Walcriz <walcriz.mail@gmail.com>
2023-10-16 19:33:09 +0200

    feat: Fix errors on startup by using changes upstream. (LazyVim)

--- lua/util/init.lua
+++ lua/util/init.lua
@@ -82,6 +82,23 @@ function M.get_root()
 	return root
 end
 
+function M.get_clients(opts)
+	local ret = {} ---@type lsp.Client[]
+	if vim.lsp.get_clients then
+		ret = vim.lsp.get_clients(opts)
+	else
+		---@diagnostic disable-next-line: deprecated
+		ret = vim.lsp.get_active_clients(opts)
+		if opts and opts.method then
+			---@param client lsp.Client
+			ret = vim.tbl_filter(function(client)
+				return client.supports_method(opts.method, { bufnr = opts.bufnr })
+			end, ret)
+		end
+	end
+	return opts and opts.filter and vim.tbl_filter(opts.filter, ret) or ret
+end
+
 -- this will return a function that calls telescope.
 -- cwd will default to util.get_root
 -- for `files`, git_files or find_files will be chosen depending on .git

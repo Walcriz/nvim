9049f3cfcf1feef4c370c73f0178df24149102cc
Walcriz <walcriz.mail@gmail.com>
2023-12-13 13:26:15 +0100

    feat: make treesitter not install typescript auto

--- lua/plugins/lang/typescript.lua
+++ lua/plugins/lang/typescript.lua
@@ -1,15 +1,5 @@
 return {
 
-	-- add typescript to treesitter
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			if type(opts.ensure_installed) == "table" then
-				vim.list_extend(opts.ensure_installed, { "typescript", "tsx" })
-			end
-		end,
-	},
-
 	-- correctly setup lspconfig
 	{
 		"neovim/nvim-lspconfig",

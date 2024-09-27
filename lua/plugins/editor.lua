9215e4afdfff45e7e73ebd77734d07557ec89455
Walcriz <walcriz.mail@gmail.com>
2024-09-27 16:51:21 +0200

    chore: make config comply with which-key 3.13.3

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -51,6 +51,9 @@ return {
 	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
+		dependencies = {
+			"echasnovski/mini.icons",
+		},
 		opts = {
 			plugins = { spelling = true },
 		},
@@ -58,31 +61,31 @@ return {
 			local wk = require("which-key")
 			wk.setup(opts)
 			local keymaps = {
-				mode = { "n", "v" },
-				["g"] = { name = "+goto" },
-				["]"] = { name = "+next" },
-				["["] = { name = "+prev" },
-				["<leader><tab>"] = { name = "+tabs" },
-				["<leader>b"] = { name = "+buffer" },
-				["<leader>c"] = { name = "+code" },
-				["<leader>f"] = { name = "+file/find" },
-				["<leader>g"] = { name = "+git" },
-				["<leader>gh"] = { name = "+hunks" },
-				["<leader>s"] = { name = "+search" },
-				["<leader>u"] = { name = "+ui" },
-				["<leader>w"] = { name = "+windows" },
-				["<leader>x"] = { name = "+diagnostics/quickfix" },
-				["<leader>n"] = { name = "+harpoon" },
-				["<leader>r"] = { name = "+quick refactorings" },
-				["<leader>o"] = { name = "+server actions" },
-				["<leader>t"] = { name = "+tools" },
-				["<leader>d"] = { name = "+code actions" },
-				["<leader>gp"] = { name = "+preview" },
+      mode = { "n", "v" },
+				{ "<leader><tab>", group = "tabs" },
+				{ "<leader>b", group = "buffer" },
+				{ "<leader>c", group = "code" },
+				{ "<leader>d", group = "code actions" },
+				{ "<leader>f", group = "file/find" },
+				{ "<leader>g", group = "git" },
+				{ "<leader>gh", group = "hunks" },
+				{ "<leader>gp", group = "preview" },
+				{ "<leader>n", group = "harpoon" },
+				{ "<leader>o", group = "server actions" },
+				{ "<leader>r", group = "quick refactorings" },
+				{ "<leader>s", group = "search" },
+				{ "<leader>t", group = "tools" },
+				{ "<leader>u", group = "ui" },
+				{ "<leader>w", group = "windows" },
+				{ "<leader>x", group = "diagnostics/quickfix" },
+				{ "[", group = "prev" },
+				{ "]", group = "next" },
+				{ "g", group = "goto" },
 			}
 			if Util.has("noice.nvim") then
 				keymaps["<leader>sn"] = { name = "+noice" }
 			end
-			wk.register(keymaps)
+			wk.add(keymaps)
 		end,
 	},
 

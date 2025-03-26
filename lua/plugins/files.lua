7be47c70371f9a50e29fd55b42a176252c2a89b9
Walcriz <68862100+Walcriz@users.noreply.github.com>
2025-03-26 17:20:32 +0100

    fix: tree not remembering state

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -260,6 +260,11 @@ return {
 			filters = {
 				custom = { "^.git$" }
 			},
+			actions = {
+				open_file = {
+					resize_window = false, -- don't resize window when opening file
+				},
+			}
 		},
 
 		config = function(_, opts)
@@ -295,6 +300,34 @@ return {
 			})
 
 			local api = require("nvim-tree.api")
+			local view = require('nvim-tree.view')
+			local augroup = vim.api.nvim_create_augroup
+			local autocmd = vim.api.nvim_create_autocmd
+
+			-- save nvim-tree window width on WinResized event
+			augroup('save_nvim_tree_width', { clear = true })
+			autocmd('WinResized', {
+				group = 'save_nvim_tree_width',
+				pattern = '*',
+				callback = function()
+					local filetree_winnr = view.get_winnr()
+					if filetree_winnr ~= nil and vim.tbl_contains(vim.v.event['windows'], filetree_winnr) then
+						vim.t['filetree_width'] = vim.api.nvim_win_get_width(filetree_winnr)
+					end
+				end,
+			})
+
+			-- restore window size when openning nvim-tree
+			api.events.subscribe(api.events.Event.TreeOpen, function()
+				if vim.t['filetree_width'] ~= nil then
+					view.resize(vim.t['filetree_width'])
+				end
+			end)
+
+			vim.keymap.set('n', '<leader>e', function()
+				api.tree.find_file({ open = true, focus = true })
+			end)
+
 			api.events.subscribe(api.events.Event.FileCreated, function(file) vim.cmd("edit " .. file.fname) end)
 		end
 	},

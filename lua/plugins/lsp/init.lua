320d71da9addacf38b952497b8dd0978df71eb09
Walcriz <68862100+Walcriz@users.noreply.github.com>
2025-05-22 20:48:19 +0200

    chore: disable auto install for configured lsp's

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -115,20 +115,17 @@ return {
       local mlsp = require("mason-lspconfig")
       local available = mlsp.get_available_servers()
 
-      local ensure_installed = {} ---@type string[]
       for server, server_opts in pairs(servers) do
         if server_opts then
           server_opts = server_opts == true and {} or server_opts
           -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
           if server_opts.mason == false or not vim.tbl_contains(available, server) then
             setup(server)
-          else
-            ensure_installed[#ensure_installed + 1] = server
           end
         end
       end
 
-      mlsp.setup({ ensure_installed = ensure_installed })
+      mlsp.setup({ automatic_enable = true })
     end,
   },
 

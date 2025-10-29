b6ff67d8ed9d7cbaf0774d40d1939bab96c56dce
Walcriz <68862100+Walcriz@users.noreply.github.com>
2025-10-29 15:47:17 +0100

    feat: add pdfreader

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -400,4 +400,17 @@ return {
       'leafo/magick',
     },
   },
+
+  {
+    "r-pletnev/pdfreader.nvim",
+    lazy   = false,
+    dependencies = {
+      "folke/snacks.nvim", -- image rendering
+      "nvim-telescope/telescope.nvim", -- pickers
+
+    },
+    config = function()
+      require("pdfreader").setup()
+    end,
+  }
 }

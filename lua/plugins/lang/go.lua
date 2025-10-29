8ba8846155f50c5d4a900c353c44bc3e78475d63
Walcriz <68862100+Walcriz@users.noreply.github.com>
2025-10-29 18:46:26 +0100

    chore(format): go.lua

--- lua/plugins/lang/go.lua
+++ lua/plugins/lang/go.lua
@@ -1,43 +1,44 @@
 return {
-	{
-		"ray-x/go.nvim",
-		dependencies = {
-			"ray-x/guihua.lua",
-			"neovim/nvim-lspconfig",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		config = true,
-		ft = { "go", "gomod" },
-		build = function() require("go.install").update_all() end
-	},
+  {
+    "ray-x/go.nvim",
+    branch = "treesitter-main",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = true,
+    ft = { "go", "gomod" },
+    build = function() require("go.install").update_all() end
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				gopls = {
-					mason = false,
-					staticcheck = true,
-				},
-			},
-		},
-	},
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          mason = false,
+          staticcheck = true,
+        },
+      },
+    },
+  },
 
-	{
-		"Walcriz/action-lists.nvim",
-		opts = {
-			lists = {
-				code_insert = {
-					go = {
-						ft = { "go" },
-						actions = {
-							Comment = "GoCmt",
-							["Tag Add"] = "GoAddTag",
-							["Tag Rm"] = "GoRmTag",
-						},
-					},
-				},
-			},
-		},
-	},
+  {
+    "Walcriz/action-lists.nvim",
+    opts = {
+      lists = {
+        code_insert = {
+          go = {
+            ft = { "go" },
+            actions = {
+              Comment = "GoCmt",
+              ["Tag Add"] = "GoAddTag",
+              ["Tag Rm"] = "GoRmTag",
+            },
+          },
+        },
+      },
+    },
+  },
 }

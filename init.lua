b6731591b4e1d14d5bf18a7cfa3fc60c47dad7ab
Walcriz <68862100+Walcriz@users.noreply.github.com>
2025-12-21 16:22:04 +0100

    chore: fix indentation in init.lua

--- init.lua
+++ init.lua
@@ -2,10 +2,10 @@ local init = require("init")
 
 -- Avoid freezing vim forever
 if vim.fn.has("win32") then
-	vim.keymap.set("n", "<C-z>", "<Nop>", { noremap = true })
-	vim.keymap.set("i", "<C-c>", "<esc>", { noremap = true })
-	vim.keymap.set("n", "<C-c>", "<esc>", { noremap = true })
-	vim.keymap.set("v", "<C-c>", "<esc>", { noremap = true })
+  vim.keymap.set("n", "<C-z>", "<Nop>", { noremap = true })
+  vim.keymap.set("i", "<C-c>", "<esc>", { noremap = true })
+  vim.keymap.set("n", "<C-c>", "<esc>", { noremap = true })
+  vim.keymap.set("v", "<C-c>", "<esc>", { noremap = true })
 end
 
 -- Initial setup and bootstrapping

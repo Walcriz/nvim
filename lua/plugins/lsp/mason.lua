746a2948e915154c567508afb80a4e8c3aa52f58
Walcriz <68862100+Walcriz@users.noreply.github.com>
2026-06-14 12:06:33 +0200

    feat: better guard if a package exists or not for mason

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -15,9 +15,13 @@ return {
       local mr = require("mason-registry")
       require("plugins.lang._lazyload").setup(require("plugins.lang._loader").load().langs)
       for _, tool in ipairs(opts.ensure_installed) do
-        local p = mr.get_package(tool)
-        if not p:is_installed() then
-          p:install()
+        if mr.has_package(tool) then
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
+        else
+          vim.notify(("mason: unknown package '%s'"):format(tool), vim.log.levels.WARN)
         end
       end
     end,

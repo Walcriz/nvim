f065a013223f89a9569943c856b9e1d42544da22
Walcriz <68862100+Walcriz@users.noreply.github.com>
2026-06-14 12:04:38 +0200

    fix: friendly-snippets not loaded correctly

--- lua/plugins/lsp/lua_snip.lua
+++ lua/plugins/lsp/lua_snip.lua
@@ -4,10 +4,12 @@ return {
     enabled = vim.g.walcriz.core.lsp and not vim.g.walcriz.core.minimal,
     build = (not jit.os:find("Windows")) and "make install_jsregexp" or nil,
     dependencies = {
-      "rafamadriz/friendly-snippets",
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
-      end,
+      {
+        "rafamadriz/friendly-snippets",
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+      },
     },
     opts = {
       delete_check_events = { "CursorMoved", "CursorMovedI" },

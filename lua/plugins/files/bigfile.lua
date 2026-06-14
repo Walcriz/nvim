379d90ee26fc446bdbb9f618fdec97a099e2a4e8
Walcriz <68862100+Walcriz@users.noreply.github.com>
2026-06-14 13:16:25 +0200

    fix: bigfile.nvim never loaded

--- lua/plugins/files/bigfile.lua
+++ lua/plugins/files/bigfile.lua
@@ -2,6 +2,7 @@ return {
   {
     "LunarVim/bigfile.nvim",
     enabled = not vim.g.walcriz.core.minimal,
+    event = "BufReadPre",
     opts = {},
     config = function(_, opts)
       require("bigfile").setup(opts)

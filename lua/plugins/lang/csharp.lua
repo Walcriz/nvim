9e92dba4edc6996aa0f58a4b70a1bad2416fa9c0
Walcriz <68862100+Walcriz@users.noreply.github.com>
2026-06-14 12:13:12 +0200

    fix: align setup with loader’s handler signature for csharp lang

--- lua/plugins/lang/csharp.lua
+++ lua/plugins/lang/csharp.lua
@@ -6,25 +6,22 @@ return {
       FormattingOptions = {
         OrganizeImports = true,
       },
-    }
+    },
   },
 
-  setup = function()
-    require("util").on_attach(function(client, buffer)
-      if client.name == "omnisharp" or client.name == "omnisharp_mono" then
-        client.server_capabilities.semanticTokensProvider = nil
-
-        vim.keymap.set(
-          "n",
-          "gd",
-          "<cmd>lua require('omnisharp_extended').lsp_definitions()<cr>",
-          { noremap = false, buffer = buffer, desc = "Goto definition" }
-        )
-      end
-    end)
+  setup = function(client, buffer)
+    if client.name == "omnisharp" or client.name == "omnisharp_mono" then
+      client.server_capabilities.semanticTokensProvider = nil
+      vim.keymap.set(
+        "n",
+        "gd",
+        "<cmd>lua require('omnisharp_extended').lsp_definitions()<cr>",
+        { noremap = false, buffer = buffer, desc = "Goto definition" }
+      )
+    end
   end,
 
   dependencies = {
-    "Hoffs/omnisharp-extended-lsp.nvim"
-  }
+    "Hoffs/omnisharp-extended-lsp.nvim",
+  },
 }

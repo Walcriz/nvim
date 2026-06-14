55ac7556f0d4753fe85f980e7a063f3a1a40db8b
Walcriz <68862100+Walcriz@users.noreply.github.com>
2026-06-14 12:17:36 +0200

    fix: d.enabled closure over loop variable lang

--- lua/plugins/lsp/lang.lua
+++ lua/plugins/lsp/lang.lua
@@ -3,21 +3,22 @@ local data = loader.load()
 
 local deps = {}
 for _, lang in ipairs(data.langs) do
+  local lang_spec = lang
   for _, dep in ipairs(lang.dependencies or {}) do
     local d = type(dep) == "table" and vim.deepcopy(dep) or { dep }
 
     local existing_enabled = d.enabled
-    d.enabled = function()
+    d.enabled = function(...)
       if existing_enabled ~= nil then
         local ex = existing_enabled
-        if type(ex) == "function" then
-          ex = ex()
+        if type(existing_enabled) == "function" then
+          ex = existing_enabled(...)
         end
         if not ex then
           return false
         end
       end
-      return loader.is_enabled(lang.lsp, lang)
+      return loader.is_enabled(lang_spec.lsp, lang_spec)
     end
 
     table.insert(deps, d)

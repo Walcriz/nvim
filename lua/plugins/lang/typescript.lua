c79a12b3d3c40f35f1767400c3c9cf4f2eeb02fa
Walcriz <68862100+Walcriz@users.noreply.github.com>
2026-06-14 12:18:35 +0200

    fix: use the language setup(client, buffer) hook directly instead of registering another LspAttach autocmd in typescript lang

--- lua/plugins/lang/typescript.lua
+++ lua/plugins/lang/typescript.lua
@@ -1,15 +1,13 @@
 return {
   lsp = "tsserver",
 
-  setup = function()
-    require("common").on_attach(function(client, buffer)
-      if client.name == "tsserver" then
-        -- stylua: ignore
-        vim.keymap.set("n", "<leader>ro", "<cmd>TypescriptOrganizeImports<CR>",
-          { buffer = buffer, desc = "Organize Imports" })
-        -- stylua: ignore
-        vim.keymap.set("n", "<leader>rR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = buffer })
-      end
-    end)
+  setup = function(client, buffer)
+    if client.name == "tsserver" then
+      -- stylua: ignore
+      vim.keymap.set("n", "<leader>ro", "<cmd>TypescriptOrganizeImports<CR>",
+        { buffer = buffer, desc = "Organize Imports" })
+      -- stylua: ignore
+      vim.keymap.set("n", "<leader>rR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = buffer })
+    end
   end,
 }
